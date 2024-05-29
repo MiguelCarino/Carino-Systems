f64ac89c16f90e5d269ca757a33c5950d82b7cc3
MiguelCarino <miguel.carino1994@outlook.com>
2024-05-29 17:06:36 -0600

    added waybar.tar.xz and wofi.tar.xz

--- setup.sh
+++ setup.sh
@@ -633,7 +633,7 @@ finalTweaks ()
     ;;
     *hyprland*)
     curl -s https://raw.githubusercontent.com/MiguelCarino/Carino-Systems/main/profiles/hyprland.conf > ~/.config/hypr/hyprland.conf
-    curl -s https://github.com/MiguelCarino/Carino-Systems/tree/d8b71300679cdbd8f26be93307c6679ff59bbe3a/profiles/waybar > ~/.config/hypr/hyprland.conf
+    wget https://github.com/MiguelCarino/Carino-Systems/blob/main/profiles/hyprland.tar.xz && tar -xvf hyprland.tar.xz --directory=~/.config/
     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
     xdg-mime default thunar.desktop inode/directory
     mkdir ~/.config/hypr/
