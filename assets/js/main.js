4f652fa6779f2bb14a0d10d8e27bef11e7551d41
MiguelCarino <miguel.carino1994@outlook.com>
2024-06-15 22:28:10 -0600

    simplified footer, removed multiple unused pages, changed music grid height

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,4 +114,22 @@
         document.getElementById("random-phrase").textContent = t
     };
 
+    document.addEventListener("DOMContentLoaded", function() {
+        var copyright = document.getElementById("copyright");
+        if (copyright) {
+            copyright.innerHTML = `
+                <ul class="copyright">
+                    <li><b>&copy; Carino Systems</b></li>
+                    <li>Design: a modified <a href="https://html5up.net">HTML5 UP</a> template</li>
+                    <li>
+                        <a href="https://github.com/MiguelCarino" class="icon brands alt fa-github"><span class="label">Github</span></a>
+                        <a href="https://www.linkedin.com/in/miguelcarino94" class="icon brands alt fa-linkedin"><span class="label">LinkedIn</span></a>
+                        <a href="https://mastodon.social/@miguelcarino" class="icon brands alt fa-mastodon"><span class="label">Mastodon</span></a>
+                    </li>
+                </ul>
+            `;
+        }
+    });
+    
+
 }(jQuery);
