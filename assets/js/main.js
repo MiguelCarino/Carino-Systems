463a01a1a12b07255febd657c9b960ae3b31cd4d
MiguelCarino <miguel.carino1994@outlook.com>
2024-06-16 02:55:33 -0600

    re added takwt and code optimization

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,19 +152,20 @@
             `;
         }
     });
-    
-    function loadTawkToChat() {
-        var Tawk_API = Tawk_API || {},
-            Tawk_LoadStart = new Date();
-        (function() {
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/655c3bf8d600b968d31553e3/1hfo4kh3h';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    }
 
+
+}(jQuery);
+
+function loadTawkToChat() {
+    var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+    (function() {
+        var s1 = document.createElement("script"),
+            s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/655c3bf8d600b968d31553e3/1hfo4kh3h';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
 }(jQuery);
