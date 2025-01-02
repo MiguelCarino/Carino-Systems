ddf46054e499d9915ea572d0b5ab93a058672ac9
MiguelCarino <miguel.carino1994@outlook.com>
2025-01-02 03:38:48 -0600

    removed footer content, mastodon link, and added notable-people link

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,19 +35,13 @@ document.addEventListener("DOMContentLoaded", function() {
             <li><a href="https://www.linkedin.com/in/miguelcarino94" target="_blank" class="icon-link">
                 <i class="fab fa-linkedin"></i> <span class="label"></span>
             </a></li>
-            <li><a href="https://mastodon.social/@miguelcarino" target="_blank" class="icon-link">
-                <i class="fab fa-mastodon"></i> <span class="label"></span>
-            </a></li>
-            <li><a href="https://steamcommunity.com/id/HabiOS/" target="_blank" class="icon-link">
-                <i class="fab fa-steam"></i> <span class="label"></span>
-            </a></li>
         </ul>
     `;
     document.getElementById('navbar').innerHTML = navContent;
 
     // Footer content
     const footerContent = `
-         <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="index.html">This website</a> is marked with <a href="https://creativecommons.org/publicdomain/zero/1.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC0 1.0 Universal<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/zero.svg?ref=chooser-v1" alt=""></a></p> 
+         <!--<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="index.html">This website</a> is marked with <a href="https://creativecommons.org/publicdomain/zero/1.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC0 1.0 Universal<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/zero.svg?ref=chooser-v1" alt=""></a></p> -->
     `;
     document.getElementById('footer').innerHTML = footerContent;
 });
