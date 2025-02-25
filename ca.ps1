a9ab69e1f3bcca3185ad7df0e9470487333c9c09
MiguelCarino <miguel.carino1994@outlook.com>
2025-02-25 15:41:00 -0600

    forcing string as argument

--- ca.ps1
+++ ca.ps1
@@ -5,23 +5,23 @@ param (
 function Invoke-ScriptFromURL {
     param (
         [string]$Url,
-        [string[]]$Arguments
+        [string]$Arguments
     )
     
     $ScriptContent = Invoke-WebRequest -Uri $Url -UseBasicParsing | Select-Object -ExpandProperty Content
-    Invoke-Expression $ScriptContent @Arguments
+    Invoke-Expression "$ScriptContent $Arguments"
 }
 
 if ($Arg1) {
     switch -Wildcard ($Arg1) {
         "*setup*" {
-            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleSetup/setup.ps1" -Arguments $args
+            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleSetup/setup.ps1" -Arguments ($args -join " ")
         }
         "*vp9*" {
-            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments $args
+            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments ($args -join " ")
         }
         "*av1*" {
-            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments $args
+            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments ($args -join " ")
         }
         default {
             Write-Host "No valid option provided."
