9dcf39becc7d4c48e53026e4a3f6f74ecda7f295
MiguelCarino <miguel.carino1994@outlook.com>
2025-02-25 15:47:18 -0600

    updated arguments

--- ca.ps1
+++ ca.ps1
@@ -1,5 +1,6 @@
 param (
-    [string]$Arg1
+    [string]$Arg1,
+    [string[]]$AdditionalArgs  # Collects all additional arguments
 )
 
 function Invoke-ScriptFromURL {
@@ -10,25 +11,25 @@ function Invoke-ScriptFromURL {
 
     $TempScript = [System.IO.Path]::GetTempFileName() + ".ps1"
     Invoke-WebRequest -Uri $Url -UseBasicParsing | Select-Object -ExpandProperty Content | Set-Content -Path $TempScript
-    & $TempScript @Arguments  # Execute script safely
+    & $TempScript @Arguments  # Execute the script with arguments
     Remove-Item -Path $TempScript -Force  # Clean up
 }
 
 if ($Arg1) {
     switch -Wildcard ($Arg1) {
         "*setup*" {
-            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleSetup/setup.ps1" -Arguments ($args -join " ")
+            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleSetup/setup.ps1" -Arguments ($AdditionalArgs -join " ")
         }
         "*vp9*" {
-            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments ($args -join " ")
+            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments ($AdditionalArgs -join " ")
         }
         "*av1*" {
-            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments ($args -join " ")
+            Invoke-ScriptFromURL -Url "https://miguelcarino.github.io/SimpleTranscoding/transcode.ps1" -Arguments ($AdditionalArgs -join " ")
         }
         "test" {
             if (Test-Path "./test.ps1") {
                 Write-Host "Running test.ps1..."
-                & "./test.ps1"
+                & "./test.ps1" @AdditionalArgs
             } else {
                 Write-Host "test.ps1 not found!"
             }
